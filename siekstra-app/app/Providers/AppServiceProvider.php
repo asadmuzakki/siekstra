21d65b6b4242aa8c8278b11dcfbbfabd66e77a05
asadmuzakki <asadmuzakki90@gmail.com>
2025-06-14 18:01:32 +0800

    add macro to Request for safe hostname retrieval

--- siekstra-app/app/Providers/AppServiceProvider.php
+++ siekstra-app/app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,8 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        // Tambahkan macro untuk menghindari gethostbyaddr()
+        Request::macro('getHostNameSafe', function () {
+            return $_SERVER['REMOTE_ADDR'] ?? 'localhost';
         });
     }
 }
