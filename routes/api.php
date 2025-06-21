9c0de37d978cde0d64e13442d48088311511c7f5
asadmuzakki <asadmuzakki90@gmail.com>
2025-06-21 23:52:21 +0800

    Refactor routes to simplify NilaiController reference and ensure consistent formatting

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Api\TesController;
 use App\Http\Controllers\Api\AbsensiController;
 use App\Http\Controllers\Api\KegiatanController;
 use App\Http\Controllers\Api\AbsensiTutorController;
+use App\Http\Controllers\Api\NilaiController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -35,4 +36,5 @@ Route::apiResource('absensi', AbsensiController::class);
 Route::get('/riwayat-absensi/{ekskul_id}', [AbsensiController::class, 'rekap']);
 Route::get('/riwayat-kegiatan/{ekskul_id}', [KegiatanController::class, 'rekap']);
 Route::apiResource('absensi-tutor', AbsensiTutorController::class);
-Route::apiResource('nilais', \App\Http\Controllers\Api\NilaiController::class);
+Route::apiResource('nilais',NilaiController::class);
+Route::apiResource('kegiatan', KegiatanController::class);
