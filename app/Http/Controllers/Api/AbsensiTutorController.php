641c21cfb692dbc378c4f26435a6546e85ab2425
asadmuzakki <asadmuzakki90@gmail.com>
2025-06-25 15:18:43 +0800

    Fix ordering of Absensi Tutor records in index method

--- app/Http/Controllers/Api/AbsensiTutorController.php
+++ app/Http/Controllers/Api/AbsensiTutorController.php
@@ -14,7 +14,7 @@ class AbsensiTutorController extends Controller
      */
     public function index()
     {
-        $absensiTutors = AbsensiTutor::all();
+        $absensiTutors = AbsensiTutor::all()->orderBy('id', 'asc');
         $absensiTutors->load('tutor', 'ekskul'); // Eager load related models
         return new AbsensiTutorResource(true, 'List of Absensi Tutor', $absensiTutors);
     }
