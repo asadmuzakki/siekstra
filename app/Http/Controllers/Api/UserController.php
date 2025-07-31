81c0a913de0470dbc0dbd7ec464de80aa2998b90
asadmuzakki <asadmuzakki90@gmail.com>
2025-07-31 21:39:34 +0800

    Refactor UserController methods for improved clarity and consistency

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -75,7 +75,11 @@ class UserController extends Controller
     public function getTutors()
     {
         $tutors = User::role('tutor')->get();
-        return response()->json(['data' => $tutors], 200);
+        return response()->json([
+            'success' => true,
+            'message' => 'List of Tutors',
+            'data' => $tutors,
+        ], 200);
     }
     // Method untuk mendapatkan detail tutor
     public function getTutor($id)
