86b0bc04fd998b37c14b7f2a48b7cb40e7ec0d24
asadmuzakki <asadmuzakki90@gmail.com>
2025-08-17 21:21:17 +0800

    Add logout route to authentication API

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\Api\UserController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::middleware(['auth:sanctum', 'role:admin'])->prefix('admin')->group(function () {
     Route::get('/dashboard', function () {
